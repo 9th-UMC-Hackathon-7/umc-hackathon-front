1b18989612494fd947af575c134e7bec756aa4a3
Daeun Lee <dev.leedaeun@gmail.com>
2026-01-11 01:00:56 +0900

    [feat]bottomsheet 추가

--- app/src/main/java/com/example/umc_hackathon_9/MainActivity.kt
+++ app/src/main/java/com/example/umc_hackathon_9/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.umc_hackathon_9
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val relationBottomSheet = RelationBottomSheet()
+        relationBottomSheet.show(supportFragmentManager, relationBottomSheet.tag)
     }
 }
